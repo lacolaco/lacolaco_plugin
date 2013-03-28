253f29801e2c1892da55c6df1df11a03a41ab809
Toshiaki Asai <toshi.alternative@gmail.com>
2013-03-28 23:48:17 +0900

    fix indent width

--- lacolaco_plugin.rb
+++ lacolaco_plugin.rb
@@ -3,26 +3,26 @@
 Plugin.create(:lacolaco_plugin) do
 
     command(:lacolaco_plugin,
-        name: "らこらこする",
-        visible: false,
-        role: :window,
-        condition: lambda{ |opt| true }
-        ) do |opt|
+            name: "らこらこする",
+            visible: false,
+            role: :window,
+            condition: lambda{ |opt| true }
+            ) do |opt|
         lacolaco_w
     end
 
-  def lacolaco_w
-    Service.primary.update(:message => "らこらこらこ〜" + sign).trap{
-      activity :system, "失敗しました"
-    }
-  end
+    def lacolaco_w
+        Service.primary.update(:message => "らこらこらこ〜" + sign).trap{
+            activity :system, "失敗しました"
+        }
+    end
 
-  def sign
-    "ｗ" * (sign_counter.call % 8 + 1)
-  end
+    def sign
+        "ｗ" * (sign_counter.call % 8 + 1)
+    end
 
-  def sign_counter
-    @sign_counter ||= gen_counter
-  end
+    def sign_counter
+        @sign_counter ||= gen_counter
+    end
 
 end
