fc9d32470f9e88167c3397bb94e616eed4e96c81
laco0416 <laco0416@gmail.com>
2013-03-28 21:57:24 +0900

    とりあえず作ってみた

--- lacolaco_plugin.rb
+++ lacolaco_plugin.rb
@@ -1,5 +1,22 @@
 # -*- coding: utf-8 -*-
 
-Plugin.create(:test_plugin) do
-	
+Plugin.create(:lacolaco_plugin) do
+    command(:lacolaco_plugin,
+        name: "らこらこする",
+        visible: false,
+        role: :window,
+        condition: lambda{ |opt| true }
+        ) do |opt|
+            Service.primary.update(:message => "らこらこらこ〜ｗ").trap{
+                Service.primary.update(:message => "らこらこらこ〜ｗｗ").trap{
+                    Service.primary.update(:message => "らこらこらこ〜ｗｗｗ").trap{
+                        Service.primary.update(:message => "らこらこらこ〜ｗｗｗｗ").trap{
+                            Service.primary.update(:message => "らこらこらこ〜ｗｗｗｗｗ").trap{
+                                Plugin.call(:message, nil, [Message.new(:message => "失敗しました", :system => true)])
+                            }
+                        }
+                    }
+                }
+            }
+    end
 end
