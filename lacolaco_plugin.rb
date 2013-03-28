6261c5bfe4e30e4b26c68956071f8699068b9dbe
Toshiaki Asai <toshi.alternative@gmail.com>
2013-03-28 23:31:01 +0900

    add らこらこらこ〜ｗ method

--- lacolaco_plugin.rb
+++ lacolaco_plugin.rb
@@ -1,22 +1,19 @@
 # -*- coding: utf-8 -*-
 
 Plugin.create(:lacolaco_plugin) do
+  
     command(:lacolaco_plugin,
         name: "らこらこする",
         visible: false,
         role: :window,
         condition: lambda{ |opt| true }
         ) do |opt|
-            Service.primary.update(:message => "らこらこらこ〜ｗ").trap{
-                Service.primary.update(:message => "らこらこらこ〜ｗｗ").trap{
-                    Service.primary.update(:message => "らこらこらこ〜ｗｗｗ").trap{
-                        Service.primary.update(:message => "らこらこらこ〜ｗｗｗｗ").trap{
-                            Service.primary.update(:message => "らこらこらこ〜ｗｗｗｗｗ").trap{
-                                Plugin.call(:message, nil, [Message.new(:message => "失敗しました", :system => true)])
-                            }
-                        }
-                    }
-                }
-            }
+        lacolaco_w
     end
+
+  def lacolaco_w
+    Service.primary.update(:message => "らこらこらこ〜ｗ").trap{
+      Plugin.call(:message, nil, [Message.new(:message => "失敗しました", :system => true)])
+    }
+  end
 end
